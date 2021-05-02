5407a8a3cc539654d3d08e1927d6bd3961dc9ce8
Carlos Tasada <carlos.tasada@mollie.com>
2021-05-02 16:13:34 +0200

    Finally using TOTP to simplify login

--- degiro/utils/degiro.py
+++ degiro/utils/degiro.py
@@ -17,12 +17,13 @@ class DeGiro():
         username = config_dict['username']
         password = config_dict['password']
         int_account = config_dict['int_account']
-        one_time_password = config_dict['one_time_password']
+        # FIXME: Using Totp is convenient, but not secure
+        totp_secret_key = config_dict['totp_secret_key']
         credentials = Credentials(
             int_account=int_account,
             username=username,
             password=password,
-            one_time_password=one_time_password,
+            totp_secret_key=totp_secret_key,
         )
 
         # SETUP TRADING API
@@ -33,3 +34,26 @@ class DeGiro():
     
     def getClient(self) -> TradingAPI:
         return self.trading_api
+
+    def _get_account_info(self):
+        return self.trading_api.get_account_info()
+
+    def _get_client_details(self):
+        # FETCH CONFIG TABLE
+        client_details_table = self.trading_api.get_client_details()
+
+        # EXTRACT DATA
+        int_account = client_details_table['data']['intAccount']
+        user_token = client_details_table['data']['id']
+        client_details_pretty = json.dumps(
+            client_details_table,
+            sort_keys=True,
+            indent=4,
+        )
+
+        return client_details_table
+    
+    def _get_config(self):
+        config_table = self.trading_api.get_config()
+
+        return config_table
--- degiro/views/account.py
+++ degiro/views/account.py
@@ -8,36 +8,12 @@ import json
 
 class Account(View):
     def __init__(self):
-        deGiro = DeGiro()
-
-        self.trading_api = deGiro.getClient()
+        self.deGiro = DeGiro()
 
     def get(self, request):
 
-        info = self._get_client_details()
+        # info = self._get_client_details()
+        # info = self._get_account()
+        info = self.deGiro._get_config()
 
         return JsonResponse(info)
-
-    def _get_account(self):
-        # FETCH DATA
-        account_info_table = self.trading_api.get_account_info()
-
-        # DISPLAY DATA
-        account_info_pretty = json.dumps(account_info_table, sort_keys=True, indent=4)
-        
-        return account_info_table
-
-    def _get_client_details(self):
-        # FETCH CONFIG TABLE
-        client_details_table = self.trading_api.get_client_details()
-
-        # EXTRACT DATA
-        int_account = client_details_table['data']['intAccount']
-        user_token = client_details_table['data']['id']
-        client_details_pretty = json.dumps(
-            client_details_table,
-            sort_keys=True,
-            indent=4,
-        )
-
-        return client_details_table
