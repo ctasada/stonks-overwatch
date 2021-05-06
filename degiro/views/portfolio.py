9fae6a7d98312c4856c479efb0d43781882f7b1d
Carlos Tasada <ctasada@gmail.com>
2021-05-06 19:06:48 +0200

    Starting to work in the portfolio template

---
+++ degiro/views/portfolio.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.http import JsonResponse
+from django.views import View
+from django.shortcuts import render
+
+from degiro.utils.degiro import DeGiro
+
+import json
+
+class Portfolio(View):
+    def __init__(self):
+        self.deGiro = DeGiro()
+
+    def get(self, request):
+
+        return render(request, 'portfolio.html', {})
