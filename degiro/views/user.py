dd2fe52169122270685c54316f1b8262b37cd729
Carlos Tasada <ctasada@gmail.com>
2021-05-07 16:55:44 +0200

    Basic User form. Still WIP

--- degiro/views/user.py
+++ degiro/views/user.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.http import JsonResponse
 from django.views import View
 from django.shortcuts import render
+import pycountry
 
 from degiro.utils.degiro import DeGiro
 
@@ -12,5 +13,19 @@ class User(View):
         self.deGiro = DeGiro()
 
     def get(self, request):
+        accountInfo = self.deGiro.get_account_info()
+        clientDetails = self.deGiro.get_client_details()
+        # info = self.deGiro._get_config()
+        # return JsonResponse(info)
 
-        return render(request, 'user.html', {})
+        print(clientDetails)
+
+        context = {
+            "username": clientDetails['data']['username'],
+            "clientRole": clientDetails['data']['clientRole'].capitalize(),
+            "fullName": clientDetails['data']['displayName'],
+            "dateOfBirth": clientDetails['data']['firstContact']['dateOfBirth'],
+            "nationality": pycountry.countries.get(alpha_2=clientDetails['data']['firstContact']['nationality']).name,
+        }
+
+        return render(request, 'user.html', context)
