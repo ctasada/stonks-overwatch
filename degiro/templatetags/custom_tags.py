ec05145a80e6073d94c6e17fd3accef58c1a8f23
Carlos Tasada <carlos.tasada@mollie.com>
2021-05-07 19:05:21 +0200

    Properly show the username in the sidebar

---
+++ degiro/templatetags/custom_tags.py
@@ -0,0 +1,14 @@
+from django import template
+from degiro.utils.degiro import DeGiro
+
+register = template.Library()
+
+@register.simple_tag
+def clientrole():
+    clientDetails = DeGiro().get_client_details()
+    return clientDetails['data']['clientRole'].capitalize()
+
+@register.simple_tag
+def username():
+    clientDetails = DeGiro().get_client_details()
+    return clientDetails['data']['username']
