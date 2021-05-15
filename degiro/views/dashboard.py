2e945581111d47463cd3879c27cc32c974c6cfba
Carlos Tasada <ctasada@gmail.com>
2021-05-15 20:24:26 +0200

    Added basic dashboard and refactored Portfolio to its own class

--- degiro/views/dashboard.py
+++ degiro/views/dashboard.py
@@ -1,16 +1,34 @@
-from django.http import HttpResponse
-from django.http import JsonResponse
 from django.views import View
 from django.shortcuts import render
 
-from degiro.utils.degiro import DeGiro
+from degiro.models.portfolio import PortfolioModel
 
 import json
 
 class Dashboard(View):
     def __init__(self):
-        self.deGiro = DeGiro()
+        self.portfolio = PortfolioModel()
 
     def get(self, request):
+        portfolio = self.portfolio.get_portfolio()
+        # print (json.dumps(portfolio, indent=2))
 
-        return render(request, 'dashboard.html', {})
+        sectors = {}
+
+        for stock in portfolio:
+            if stock['isOpen']:
+                sectorName = stock['sector']
+                sectors[sectorName] = stock.get("value", 0) + stock['value']
+
+        labels = []
+        values = []
+        for key in sectors:
+            labels.append(key)
+            values.append(sectors[key])
+
+        context = {
+            "labels": labels,
+            "values": values,
+        }
+
+        return render(request, 'dashboard.html', context)
