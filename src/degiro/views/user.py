2a04fb09fde9632293238f6b73bfc20200b8eeda
Carlos Tasada <ctasada@gmail.com>
2021-05-22 16:23:40 +0200

    Started User Model

--- src/degiro/views/user.py
+++ src/degiro/views/user.py
@@ -1,29 +1,24 @@
-from django.http import HttpResponse
-from django.http import JsonResponse
 from django.views import View
 from django.shortcuts import render
 import pycountry
 
-from degiro.utils.degiro import DeGiro
+from degiro.models.user import UserModel
 
 import json
 
 class User(View):
     def __init__(self):
-        self.deGiro = DeGiro()
+        self.user = UserModel()
 
     def get(self, request):
-        # accountInfo = self.deGiro.get_account_info()
-        clientDetails = self.deGiro.get_client_details()
-        #info = self.deGiro.get_config()
-        #print(clientDetails)
+        clientDetails = self.user.getDetails()
 
         context = {
-            "username": clientDetails['data']['username'],
-            "clientRole": clientDetails['data']['clientRole'].capitalize(),
-            "fullName": clientDetails['data']['displayName'],
-            "dateOfBirth": clientDetails['data']['firstContact']['dateOfBirth'],
-            "nationality": pycountry.languages.get(alpha_2=clientDetails['data']['firstContact']['nationality']).name,
+            "username": clientDetails['username'],
+            "clientRole": clientDetails['clientRole'].capitalize(),
+            "fullName": clientDetails['displayName'],
+            "dateOfBirth": clientDetails['firstContact']['dateOfBirth'],
+            "nationality": pycountry.languages.get(alpha_2=clientDetails['firstContact']['nationality']).name,
         }
 
         return render(request, 'user.html', context)
