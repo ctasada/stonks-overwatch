85ee6ade243f86bca856cd3a097ffe16117ae42d
Carlos Tasada <ctasada@gmail.com>
2021-05-23 14:50:23 +0200

    Tweaks

--- src/degiro/models/user.py
+++ src/degiro/models/user.py
@@ -1,8 +1,15 @@
 from degiro.utils.degiro import DeGiro
+import json
 
 class UserModel:
+    clientDetails = None
 
-    def getDetails(self):
+    def get_details(self):
         clientDetails = DeGiro.get_client_details()
 
         return clientDetails['data']
+
+    def get_language(self):
+        clientDetails = DeGiro.get_client_details()
+
+        return clientDetails['data']['language']
