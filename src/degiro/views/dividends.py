4b09a96195cada120168640a0e15d8d30e4c7217
Carlos Tasada <ctasada@gmail.com>
2022-02-05 10:03:10 +0100

    Dividends now include months without payouts

--- src/degiro/views/dividends.py
+++ src/degiro/views/dividends.py
@@ -1,6 +1,7 @@
 from django.views import View
 from django.shortcuts import render
 import datetime
+import pandas as pd
 
 from degiro.models.account_overview import AccountOverviewModel
 from degiro.utils.localization import LocalizationUtility
@@ -8,6 +9,9 @@ from degiro.utils.localization import LocalizationUtility
 import json
 
 class Dividends(View):
+
+    DATETIME_PATTERN = '%Y-%m-%d %H:%M:%S'
+
     def __init__(self):
         self.accountOverview = AccountOverviewModel()
 
@@ -15,7 +19,8 @@ class Dividends(View):
         # We don't need to sort the dict, since it's already coming sorted in DESC date order
         dividendsOverview = self.accountOverview.get_dividends()
 
-        dividends = dict()
+        dividends = self.get_dividends_calendar(dividendsOverview)
+
         for transaction in dividendsOverview:
             # Group dividends by month. We may only need the dividend name and amount
             month = self.format_date_to_month(transaction['date'])
@@ -48,18 +53,33 @@ class Dividends(View):
             monthEntry["total"] = total + transaction['change']
             monthEntry["formatedTotal"] = LocalizationUtility.format_money_value(value = monthEntry['total'], currency = transaction['currency'])
 
-        print(dividends)
-
         context = {
             'dividends': dividends
         }
         
         return render(request, 'dividends.html', context)
 
+    def get_dividends_calendar(self, dividendsOverview):
+        dividends = dict()
+        
+        df = pd.DataFrame(dividendsOverview)
+        periodStart = min(df['date'])
+        periodEnd = datetime.date.today()
+        period = pd.period_range(start=periodStart, end=periodEnd, freq='M')[::-1]
+
+        for month in period:
+            month = month.strftime('%B %Y')
+            monthEntry = dividends.setdefault(month, dict())
+            monthEntry.setdefault("payouts", 0)
+            monthEntry.setdefault("total", 0)
+            monthEntry.setdefault("formatedTotal", LocalizationUtility.format_money_value(value = 0, currencySymbol = LocalizationUtility.get_base_currency_symbol()
+))
+        return dividends
+
     def format_date_to_month(self, value: str):
-        time = datetime.datetime.strptime(value, '%Y-%m-%d %H:%M:%S')
+        time = datetime.datetime.strptime(value, self.DATETIME_PATTERN)
         return time.strftime('%B %Y')
 
     def get_date_day(self, value: str):
-        time = datetime.datetime.strptime(value, '%Y-%m-%d %H:%M:%S')
+        time = datetime.datetime.strptime(value, self.DATETIME_PATTERN)
         return time.strftime('%d')
