15b28cfabff8410b388df3df781400c4c1319338
Carlos Tasada <ctasada@gmail.com>
2024-01-20 20:23:37 +0100

    Simplified scripts

--- scripts/account_overview.py
+++ scripts/account_overview.py
@@ -1,7 +1,6 @@
 # IMPORTATIONS
-import datetime
+import common
 import json
-import logging
 
 from datetime import date
 
@@ -9,46 +8,18 @@ from degiro_connector.trading.api import API as TradingAPI
 from degiro_connector.trading.models.credentials import Credentials
 from degiro_connector.trading.models.account import OverviewRequest
 
-# SETUP LOGGING LEVEL
-logging.basicConfig(level=logging.DEBUG)
-
-# SETUP CONFIG DICT
-with open('./config/config.json') as config_file:
-    config_dict = json.load(config_file)
-
-# SETUP CREDENTIALS
-int_account = config_dict['int_account']
-username = config_dict['username']
-password = config_dict['password']
-totp_secret_key = config_dict['totp_secret_key']
-credentials = Credentials(
-    int_account=int_account,
-    username=username,
-    password=password,
-    totp_secret_key=totp_secret_key,
-)
-
-# SETUP TRADING API
-trading_api = TradingAPI(credentials=credentials)
-
-# CONNECT
-trading_api.connect()
+trading_api = common.connectToDegiro()
 
 # SETUP REQUEST
-today = datetime.date.today()
 from_date = date(
     year=2020,
     month=1,
     day=1,
 )
-to_date = date(
-    year=today.year,
-    month=today.month,
-    day=today.day,
-)
+
 request = OverviewRequest(
     from_date=from_date,
-    to_date=to_date,
+    to_date=date.today(),
 )
 
 # FETCH DATA
@@ -64,3 +35,5 @@ print(json.dumps(account_overview, indent = 4))
 #     if cash_movement['description'] in ['Dividend', 'Dividendbelasting']:
 #         for key, value in cash_movement.items():
 #             print(key, ' : ', value)
+
+trading_api.logout()
--- scripts/transactions.py
+++ scripts/transactions.py
@@ -1,7 +1,6 @@
 # IMPORTATIONS
-import datetime
+import common
 import json
-import logging
 
 from datetime import date
 from degiro_connector.trading.api import API as TradingAPI
@@ -9,49 +8,20 @@ from degiro_connector.trading.models.credentials import Credentials
 from degiro_connector.trading.models.product import ProductInfo
 from degiro_connector.trading.models.transaction import HistoryRequest
 
-# SETUP LOGGING LEVEL
-logging.basicConfig(level=logging.DEBUG)
-
-# SETUP CONFIG DICT
-with open('./config/config.json') as config_file:
-    config_dict = json.load(config_file)
-
-# SETUP CREDENTIALS
-int_account = config_dict['int_account']
-username = config_dict['username']
-password = config_dict['password']
-totp_secret_key = config_dict['totp_secret_key']
-credentials = Credentials(
-    int_account=int_account,
-    username=username,
-    password=password,
-    totp_secret_key=totp_secret_key,
-)
-
-# SETUP TRADING API
-trading_api = TradingAPI(credentials=credentials)
-
-# CONNECT
-trading_api.connect()
+trading_api = common.connectToDegiro()
 
 # SETUP REQUEST
-today = datetime.date.today()
 from_date = date(
     year=2020,
     month=1,
     day=1,
-)
-to_date = date(
-    year=today.year,
-    month=today.month,
-    day=today.day,
 )
 
 # FETCH TRANSACTIONS DATA
 transactions_history = trading_api.get_transactions_history(
     transaction_request=HistoryRequest(
         from_date=from_date,
-        to_date=to_date,
+        to_date=date.today(),
     ),
     raw=True,
 )
@@ -74,3 +44,5 @@ products_info = trading_api.get_products_info(
 
 # DISPLAY PRODUCTS_INFO
 print(json.dumps(products_info, indent = 4))
+
+trading_api.logout()
