0185f1286dd1e1176fc8a8d9c7e6f5c192fbd873
carlos.tasada <ctasada@gmail.com>
2024-01-29 18:52:39 +0100

    feat: Added Dividends growth graph

---
+++ scripts/agenda_dividends.py
@@ -0,0 +1,52 @@
+# IMPORTATIONS
+import common
+import json
+
+import logging
+import requests
+from datetime import datetime, timedelta
+
+from degiro_connector.trading.api import API as TradingAPI
+from degiro_connector.trading.models.credentials import build_credentials
+from degiro_connector.trading.models.agenda import AgendaRequest, CalendarType
+from degiro_connector.core.constants import urls
+
+trading_api = common.connectToDegiro()
+
+# FETCH AGENDA
+agenda = trading_api.get_agenda(
+    agenda_request=AgendaRequest(
+        calendar_type=CalendarType.DIVIDEND_CALENDAR,
+        end_date=datetime.now() + timedelta(days=30),
+        start_date=datetime.now() - timedelta(days=30),
+        offset=0,
+        limit=25,
+        company_name='IBERDROLA',
+    ),
+    raw=True,
+)
+
+# {
+#     "offset": 0,
+#     "total": 1,
+#     "items": [
+#         {
+#             "eventId": 15833294,
+#             "isin": "ES0144580Y14",
+#             "ric": "IBE.MC",
+#             "organizationName": "Iberdrola SA",
+#             "dateTime": "2024-01-09T12:00:00Z",
+#             "lastUpdate": "2024-01-09T09:48:13Z",
+#             "countryCode": "ES",
+#             "eventType": "ExDividends",
+#             "exDividendDate": "2024-01-09T12:00:00Z",
+#             "paymentDate": "2024-01-31T12:00:00Z",
+#             "dividend": 0.0,
+#             "yield": 4.62955,
+#             "currency": "EUR",
+#             "marketCap": "LARGE_CAP"
+#         }
+#     ]
+# }
+
+print(json.dumps(agenda, indent = 4))
