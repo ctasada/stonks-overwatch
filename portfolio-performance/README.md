e4b6207fbb6c2cf62f92781ce8c02d91a9b7c2f9
Carlos Tasada <ctasada@gmail.com>
2024-07-16 22:32:32 +0200

    Added Portfolio-Manager scripts. To help converting deGiro CSV files to the expected format

---
+++ portfolio-performance/README.md
@@ -0,0 +1,12 @@
+# Portfolio Performance
+
+This folder contains some scripts to help exporting the data to Portfolio Performance
+
+> Stil WIP
+
+* `deGiroAnalysis.py`: Using the `Transactions.csv` file, calculates the total costs of the Portfolio
+* `deGiroConverterAccount.py`: Converts the `Accounts.csv` file to a format that can be directly imported by Portfolio Performance
+* `deGiroConverterTrans.py`: Converts the `Transactions.csv` file to a format that can be directly imported by Portfolio Performance
+
+## TODO
+* Review the expected format to properly import the data
---
+++ portfolio-performance/deGiroConverterTrans.py
@@ -0,0 +1,37 @@
+import sys
+import os
+import pandas as pd
+from util import *
+import datetime
+
+class DeGiroConverterTrans:
+
+    def __init__(self, inputfile: str = None, data=None):
+        if data is None:
+            self.inputdata = pd.read_csv(inputfile, parse_dates=[0], date_format="%d-%m-%Y")
+        else:
+            self.inputdata = data
+        
+        self.outputdata = pd.DataFrame(
+            index=self.inputdata.index, columns=EXPORT_COLUMNS_TRANSACTIONS, data=None)
+
+    def convert(self):
+        self.outputdata['Date'] = self.inputdata['Datum'].apply(convert_date)
+        self.outputdata['Time'] = self.inputdata['Tijd']
+        self.outputdata['ISIN'] = self.inputdata['ISIN']
+        self.outputdata['Shares'] = self.inputdata['Aantal']
+        self.outputdata['Fees'] = self.inputdata.iloc[:, 14]
+        self.outputdata['Value'] = self.inputdata['Waarde'].fillna(0.0)
+        self.outputdata['Transaction Currency'] = self.inputdata.iloc[:, 10]
+        self.outputdata['Exchange Rate'] = self.inputdata['Wisselkoers']
+
+    def write_outputfile(self, outputfile: str):
+        self.outputdata.to_csv(outputfile, index=False, decimal=CSV_DECIMAL, sep=CSV_SEPARATOR)
+        print("Wrote output to: " + outputfile)
+
+
+if __name__ == '__main__':
+    converter = DeGiroConverterTrans(os.path.dirname(sys.argv[0]) + 'Transactions.csv')
+    converter.convert()
+    filename = os.path.join(os.getcwd(), 'degiro_portofolio_converted.csv')
+    converter.write_outputfile(filename)
