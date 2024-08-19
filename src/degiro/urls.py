a2efc82b4ee945b363de6d87fea3cb3360fca3de
Carlos Tasada <ctasada@gmail.com>
2024-08-19 11:28:33 +0200

    Added basic Deposits section

--- src/degiro/urls.py
+++ src/degiro/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from degiro.views.dashboard import Dashboard
 from django.views.generic import RedirectView
 from degiro.views.portfolio import Portfolio
+from degiro.views.deposits import Deposits
 from degiro.views.dividends import Dividends
 from degiro.views.transactions import Transactions
 from degiro.views.account_overview import AccountOverview
@@ -14,6 +15,7 @@ urlpatterns = [
     path("portfolio", Portfolio.as_view(), name="portfolio"),
     path("dividends", Dividends.as_view(), name="dividends"),
     path("transactions", Transactions.as_view(), name="transactions"),
+    path("deposits", Deposits.as_view(), name="deposits"),
     path("account_overview", AccountOverview.as_view(), name="account_overview"),
     path("user", User.as_view(), name="user"),
 ]
