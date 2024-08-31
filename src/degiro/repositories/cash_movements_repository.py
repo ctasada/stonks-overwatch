fdcefb0930f510ec724ccf70685750e7bf22e71f
Carlos Tasada <ctasada@gmail.com>
2024-08-31 10:26:35 +0200

    Obtain the total cash from the DB, in case DeGiro is down

--- src/degiro/repositories/cash_movements_repository.py
+++ src/degiro/repositories/cash_movements_repository.py
@@ -38,3 +38,16 @@ class CashMovementsRepository:
                 """
             )
             return cursor.fetchone()[0]
+
+    def get_total_cash(self) -> float:
+        with connection.cursor() as cursor:
+            cursor.execute(
+                """
+                SELECT balance_total
+                FROM degiro_cashmovements
+                ORDER BY id DESC
+                LIMIT 1
+                """
+            )
+            balance_total = dictfetchall(cursor)[0]['balance_total']
+            return float(balance_total)
