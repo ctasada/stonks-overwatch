ae24c70c3e8cb78264a7498b07cf0b092c190e36
Carlos Tasada <ctasada@gmail.com>
2024-09-14 16:31:04 +0200

    Replace prints by logs during the db import process

--- src/scripts/init_db.py
+++ src/scripts/init_db.py
@@ -12,11 +12,11 @@ Usage:
     poetry run src/manage.py runscript init_db --script-args companies
 """
 
+import logging
 import os
 
 from degiro.services.update_service import UpdateService
 from scripts.commons import IMPORT_FOLDER
-
 
 def init() -> None:
     """Execute needed initializations for the scripts.
@@ -28,9 +28,25 @@ def init() -> None:
     if not os.path.exists(IMPORT_FOLDER):
         os.makedirs(IMPORT_FOLDER)
 
+    # Configure logging for the stocks_portfolio module
+    stocks_portfolio_logger = logging.getLogger('stocks_portfolio')
+    stocks_portfolio_logger.setLevel(logging.INFO)
+
+    # Create a console handler
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+
+    # Create a formatter and add it to the console handler
+    format = '%(levelname)s %(asctime)s %(module)s %(message)s'
+    formatter = logging.Formatter(format)
+    console_handler.setFormatter(formatter)
+
+    # Configure logging
+    logging.basicConfig(level=logging.INFO, format=format)
+
 def account_import(update_service: UpdateService) -> None:
     """Import DeGiro Account information."""
-    print("Importing DeGiro Account Information...")
+    logging.info("Importing DeGiro Account Information...")
     update_service.update_account(
         {
             "account.json": f"{IMPORT_FOLDER}/account.json",
@@ -39,7 +55,7 @@ def account_import(update_service: UpdateService) -> None:
     )
 
 def transactions_import(update_service: UpdateService) -> None:
-    print("Importing DeGiro Transactions...")
+    logging.info("Importing DeGiro Transactions...")
     update_service.update_transactions(
         {
             "transactions.json": f"{IMPORT_FOLDER}/transactions.json"
@@ -48,13 +64,13 @@ def transactions_import(update_service: UpdateService) -> None:
 
 def products_info_import(update_service: UpdateService) -> None:
     """Import Product Information from DeGiro."""
-    print("Importing DeGiro Products Information...")
+    logging.info("Importing DeGiro Products Information...")
     update_service.update_portfolio({
         "products_info.json": f"{IMPORT_FOLDER}/products_info.json",
     })
 
 def company_profile_import(update_service: UpdateService) -> None:
-    print("Importing DeGiro Company Profiles...")
+    logging.info("Importing DeGiro Company Profiles...")
     update_service.update_company_profile({
         "company_profiles.json": f"{IMPORT_FOLDER}/company_profiles.json"
     })
