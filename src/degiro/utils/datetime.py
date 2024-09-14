21489356f503f3d38a3e1ca6eeb8610f39ed482d
Carlos Tasada <ctasada@gmail.com>
2024-09-14 08:15:41 +0200

    Code readability improvements

--- src/degiro/utils/datetime.py
+++ src/degiro/utils/datetime.py
@@ -1,79 +1,97 @@
-from datetime import date
+from datetime import date, timedelta
+from typing import Optional
 
-from dateutil.relativedelta import relativedelta
 from degiro_connector.quotecast.models.chart import Interval
 
-from degiro.utils.localization import LocalizationUtility
 
-
-def calculate_interval(date_from) -> Interval:
-    """Calculate the interval between the provided date and today.
-
-    ### Parameters
-        date_from: date from to calculate the interval
-    ### Returns
-        Interval: Interval that representes the range from date_from to today.
+class DateTimeUtility:
     """
-    # Convert String to date object
-    d1 = LocalizationUtility.convert_string_to_date(date_from)
-    today = date.today()
-    # difference between dates in timedelta
-    delta = (today - d1).days
+    A utility class for handling date and time-related operations.
+    """
 
-    interval = None
-    match delta:
-        case diff if diff in range(1, 7):
+    @staticmethod
+    def calculate_interval(date_from: str) -> Optional[Interval]:
+        """
+        Calculate the interval between the provided date and today.
+
+        Args:
+            date_from (str): The start date in ISO format (YYYY-MM-DD).
+
+        Returns:
+            Optional[Interval]: The interval representing the range from date_from to today.
+                                Returns None if the date_from is invalid or in the future.
+        """
+        d1 = date.fromisoformat(date_from)
+        today = date.today()
+
+        if d1 > today:
+            return None
+
+        delta = (today - d1).days
+
+        interval = None
+        if 1 <= delta < 7:
             interval = Interval.P1W
-        case diff if diff in range(7, 30):
+        elif 7 <= delta < 30:
             interval = Interval.P1M
-        case diff if diff in range(30, 90):
+        elif 30 <= delta < 90:
             interval = Interval.P3M
-        case diff if diff in range(90, 180):
+        elif 90 <= delta < 180:
             interval = Interval.P6M
-        case diff if diff in range(180, 365):
+        elif 180 <= delta < 365:
             interval = Interval.P1Y
-        case diff if diff in range(365, 3 * 365):
+        elif 365 <= delta < 3 * 365:
             interval = Interval.P3Y
-        case diff if diff in range(3 * 365, 5 * 365):
+        elif 3 * 365 <= delta < 5 * 365:
             interval = Interval.P5Y
-        case diff if diff in range(5 * 365, 10 * 365):
+        elif 5 * 365 <= delta < 10 * 365:
             interval = Interval.P10Y
 
-    return interval
+        return interval
 
-def calculate_dates_in_interval(from_date: date, interval: Interval) -> list:
-    # Convert values to dates
-    days = convert_interval_to_days(interval)
-    start_date = from_date - relativedelta(days=days)
-    result = []
-    for i in range(1, days):
-        day = start_date + relativedelta(days=i)
-        result.append(day)
+    @staticmethod
+    def calculate_dates_in_interval(from_date: date, interval: Interval) -> list[date]:
+        """
+        Calculate the list of dates within the given interval.
 
-    return result
+        Args:
+            from_date (date): The start date.
+            interval (Interval): The interval for which to calculate the dates.
 
-def convert_interval_to_days(interval: Interval) -> int:
-    """Convert and interval into the number of days.
+        Returns:
+            list[date]: A list of dates within the given interval.
+        """
+        days = DateTimeUtility.convert_interval_to_days(interval)
+        start_date = from_date - timedelta(days=days)
+        return [start_date + timedelta(days=i) for i in range(1, days)]
 
-    ### Parameters
-        interval: Interval
-    ### Returns
-        int: Number of days in the interval.
-    """
-    match interval:
-        case Interval.P1W:
-            return 7
-        case Interval.P1M:
-            return 30
-        case Interval.P3M:
-            return 90
-        case Interval.P6M:
-            return 180
-        case Interval.P1Y:
-            return 365
-        case Interval.P3Y:
-            return 3 * 365
-        case Interval.P5Y:
-            return 5 * 365
-        case Interval.P10Y:
-            return 10 * 365
+    @staticmethod
+    def convert_interval_to_days(interval: Interval) -> int:
+        """
+        Convert an interval into the number of days.
+
+        Args:
+            interval (Interval): The interval to convert.
+
+        Returns:
+            int: The number of days in the interval.
+        """
+        match interval:
+            case Interval.P1W:
+                return 7
+            case Interval.P1M:
+                return 30
+            case Interval.P3M:
+                return 90
+            case Interval.P6M:
+                return 180
+            case Interval.P1Y:
+                return 365
+            case Interval.P3Y:
+                return 3 * 365
+            case Interval.P5Y:
+                return 5 * 365
+            case Interval.P10Y:
+                return 10 * 365
+            case _:
+                raise ValueError(f"Invalid interval: {interval}")
--- src/degiro/utils/localization.py
+++ src/degiro/utils/localization.py
@@ -5,14 +5,20 @@ from currency_symbols import CurrencySymbols
 from degiro.config.degiro_config import DegiroConfig
 
 
-class LocalizationUtility(object):
+class LocalizationUtility:
+    """
+    A utility class for handling localization-related tasks, such as formatting dates, times, and currency values.
+    """
+
     TIME_DATE_FORMAT = "%Y-%m-%dT%H:%M:%S%z"
     DATE_FORMAT = "%Y-%m-%d"
     TIME_FORMAT = "%H:%M:%S"
 
-    # Get user's base currency
     @staticmethod
     def get_base_currency_symbol() -> str:
+        """
+        Returns the currency symbol for the user's base currency.
+        """
         base_currency = LocalizationUtility.get_base_currency()
         base_currency_symbol = CurrencySymbols.get_symbol(base_currency)
 
@@ -20,67 +26,113 @@ class LocalizationUtility(object):
 
     @staticmethod
     def get_base_currency() -> str:
+        """
+        Returns the user's base currency from the DegiroConfig.
+        """
         degiro_config = DegiroConfig.default()
         return degiro_config.base_currency
 
     @staticmethod
     def round_value(value: float) -> float:
+        """
+        Rounds a float value to 3 decimal places.
+        """
         return round(value, 3)
 
     @staticmethod
     def format_money_value(value: float, currency: str = None, currency_symbol: str = None) -> str:
+        """
+        Formats a numeric value as a currency string with the specified currency symbol.
+        If no currency symbol is provided, it uses the base currency symbol.
+        """
         if currency and not currency_symbol:
             currency_symbol = CurrencySymbols.get_symbol(currency)
+        elif not currency_symbol:
+            currency_symbol = LocalizationUtility.get_base_currency_symbol()
 
-        return currency_symbol + " {:,.2f}".format(value)
+        return f"{currency_symbol} {value:,.2f}"
 
     @staticmethod
     def format_date_time(value: str) -> str:
-        time = datetime.strptime(value, LocalizationUtility.TIME_DATE_FORMAT)
-        return time.strftime(LocalizationUtility.DATE_FORMAT + " " + LocalizationUtility.TIME_FORMAT)
+        """
+        Formats a date and time string in the specified format.
+        """
+        time = datetime.fromisoformat(value)
+        return time.strftime(f"{LocalizationUtility.DATE_FORMAT} {LocalizationUtility.TIME_FORMAT}")
+
 
     @staticmethod
     def format_date(value: str) -> str:
+        """
+        Formats a date time string to date string.
+        """
         time = datetime.strptime(value, LocalizationUtility.TIME_DATE_FORMAT)
         return time.strftime(LocalizationUtility.DATE_FORMAT)
 
     @staticmethod
     def format_time(value: str) -> str:
+        """
+        Formats a date time string to time string.
+        """
         time = datetime.strptime(value, LocalizationUtility.TIME_DATE_FORMAT)
         return time.strftime(LocalizationUtility.TIME_FORMAT)
 
     @staticmethod
     def format_date_from_date(value: datetime) -> str:
+        """
+        Formats a datetime object to a date string.
+        """
         return value.strftime(LocalizationUtility.DATE_FORMAT)
 
     @staticmethod
     def format_time_from_date(value: datetime) -> str:
+        """
+        Formats a datetime object to a time string.
+        """
         return value.strftime(LocalizationUtility.TIME_FORMAT)
 
     @staticmethod
     def format_date_to_month_year(value: str) -> str:
+        """
+        Formats a date string to a month and year string.
+        """
         time = datetime.strptime(value, LocalizationUtility.DATE_FORMAT)
         return time.strftime("%B %Y")
 
     @staticmethod
     def get_date_day(value: str) -> str:
+        """
+        Returns the day of the month from a date string.
+        """
         time = datetime.strptime(value, LocalizationUtility.DATE_FORMAT)
         return time.strftime("%d")
 
     @staticmethod
     def format_date_to_month_number(value: str) -> str:
+        """
+        Formats a date string to a month number string.
+        """
         time = datetime.strptime(value, LocalizationUtility.DATE_FORMAT)
         return time.strftime("%m")
 
     @staticmethod
     def format_date_to_year(value: str) -> str:
+        """
+        Formats a date string to a year string.
+        """
         time = datetime.strptime(value, LocalizationUtility.DATE_FORMAT)
         return time.strftime("%Y")
 
     @staticmethod
     def convert_string_to_date(value: str) -> date:
-        return datetime.strptime(value, LocalizationUtility.DATE_FORMAT).date()
+        """
+        Converts a string to a date object.
+        """
+        return datetime.fromisoformat(value).date()
 
     @staticmethod
-    def convert_string_to_datetime(value: str) -> date:
-        return datetime.strptime(value, LocalizationUtility.TIME_DATE_FORMAT)
+    def convert_string_to_datetime(value: str) -> datetime:
+        """
+        Converts a string to a datetime object.
+        """
+        return datetime.fromisoformat(value)
