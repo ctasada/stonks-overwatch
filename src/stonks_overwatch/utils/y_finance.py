fdd889a26d52fa54bad6c2f23b0920bcd75a8694
Carlos Tasada <ctasada@gmail.com>
2025-02-19 08:42:04 +0100

    Updated YFinance and added cache to avoid rate-limiting

--- src/stonks_overwatch/utils/y_finance.py
+++ src/stonks_overwatch/utils/y_finance.py
@@ -2,13 +2,19 @@ from dataclasses import dataclass
 from datetime import datetime
 from typing import List
 
+import requests_cache
 import yfinance as yf
+from requests_cache import CachedSession
 
 
 @dataclass
 class StockSplit:
     date: datetime
     split_ratio: float
+
+def __yfinance_cache() -> CachedSession:
+    return requests_cache.CachedSession('yfinance.cache')
+
 
 def get_stock_splits(ticker: str) -> List[StockSplit]:
     """Get stock splits for a given ticker
@@ -19,7 +25,7 @@ def get_stock_splits(ticker: str) -> List[StockSplit]:
     Returns:
         List[Dict[str, str]]: List of stock splits
     """
-    ticker = yf.Ticker(ticker)
+    ticker = yf.Ticker(ticker, session=__yfinance_cache())
     splits = ticker.splits
     splits_list = [StockSplit(date.to_pydatetime().astimezone(), ratio) for date, ratio in splits.to_dict().items()]
     return splits_list
