5b8022040b2169fdf7a1fef690d65828e6152d4e
Carlos Tasada <ctasada@gmail.com>
2025-02-19 19:32:05 +0100

    Added simple Dividends aggregator

---
+++ src/stonks_overwatch/services/dividends_aggregator.py
@@ -0,0 +1,38 @@
+from typing import List
+
+from stonks_overwatch.config import Config
+from stonks_overwatch.services.degiro.account_overview import AccountOverviewService as DeGiroAccountOverviewService
+from stonks_overwatch.services.degiro.currency_converter_service import (
+    CurrencyConverterService as DeGiroCurrencyConverterService,
+)
+from stonks_overwatch.services.degiro.degiro_service import DeGiroService
+from stonks_overwatch.services.degiro.dividends import DividendsService as DeGiroDividendsService
+from stonks_overwatch.services.models import AccountOverview, PortfolioId
+
+
+class DividendsAggregatorService:
+
+    def __init__(self):
+        self.degiro_service = DeGiroService()
+
+        self.account_overview = DeGiroAccountOverviewService()
+        self.currency_service = DeGiroCurrencyConverterService()
+        self.degiro_dividends = DeGiroDividendsService(
+            account_overview=self.account_overview,
+            currency_service=self.currency_service,
+            degiro_service=self.degiro_service,
+        )
+
+    def get_dividends(self, selected_portfolio: PortfolioId) -> List[AccountOverview]:
+        dividends = []
+        if Config.default().is_degiro_enabled(selected_portfolio):
+            dividends += self.degiro_dividends.get_dividends()
+
+        return sorted(dividends, key=lambda k: k.datetime, reverse=True)
+
+    def get_upcoming_dividends(self, selected_portfolio: PortfolioId) -> List[AccountOverview]:
+        dividends = []
+        if Config.default().is_degiro_enabled(selected_portfolio):
+            dividends += self.degiro_dividends.get_upcoming_dividends()
+
+        return sorted(dividends, key=lambda k: k.datetime, reverse=True)
