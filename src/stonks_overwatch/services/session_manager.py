2acf6e6445c086a1c1ec4fb5b16d1adeb62c84d4
Carlos Tasada <ctasada@gmail.com>
2025-02-24 08:38:00 +0100

    Show the portfolio selector only when needed

--- src/stonks_overwatch/services/session_manager.py
+++ src/stonks_overwatch/services/session_manager.py
@@ -1,5 +1,6 @@
 from django.http import HttpRequest
 
+from stonks_overwatch.config.config import Config
 from stonks_overwatch.services.models import PortfolioId
 
 
@@ -14,7 +15,21 @@ class SessionManager:
             portfolio_id = request.session.get(SessionManager.SESSION_SELECTED_PORTFOLIO)
             return PortfolioId.from_id(portfolio_id)
 
+        available_portfolios = SessionManager.__get_available_portfolios()
+        if len(available_portfolios) == 1:
+            return available_portfolios[0]
+
         return PortfolioId.ALL
+
+    @staticmethod
+    def __get_available_portfolios() -> list[PortfolioId]:
+        portfolios = []
+        for value in PortfolioId:
+            # Add only the enabled portfolios
+            if Config().default().is_enabled(value):
+                portfolios.append(value)
+        return portfolios
+
 
     @staticmethod
     def set_selected_portfolio(request: HttpRequest, portfolio_id: PortfolioId):
