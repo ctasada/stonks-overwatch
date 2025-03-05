f95297c16548550b1270429386ec2f5c5155d021
Carlos Tasada <ctasada@gmail.com>
2025-03-05 22:09:04 +0100

    Portfolio improvements
    When a stock sector or industry is unknown, now it tries to retrieve the information from YFinance.
    
    Also improved the Diversification information about country

--- src/stonks_overwatch/utils/y_finance.py
+++ src/stonks_overwatch/utils/y_finance.py
@@ -6,8 +6,11 @@ from typing import List
 import requests_cache
 import yfinance as yf
 from requests_cache import CachedSession
+from yfinance import Ticker
 
 import settings
+from stonks_overwatch.services.models import Country
+from stonks_overwatch.utils.constants import Sector
 
 
 @dataclass
@@ -19,6 +22,8 @@ def __yfinance_cache() -> CachedSession:
     cache_path = os.path.join(settings.TEMP_DIR, 'yfinance.cache')
     return requests_cache.CachedSession(cache_path)
 
+def __get_ticker_info(ticker: str) -> Ticker:
+    return yf.Ticker(ticker, session=__yfinance_cache())
 
 def get_stock_splits(ticker: str) -> List[StockSplit]:
     """Get stock splits for a given ticker
@@ -29,7 +34,31 @@ def get_stock_splits(ticker: str) -> List[StockSplit]:
     Returns:
         List[Dict[str, str]]: List of stock splits
     """
-    ticker = yf.Ticker(ticker, session=__yfinance_cache())
+    ticker = __get_ticker_info(ticker)
     splits = ticker.splits
     splits_list = [StockSplit(date.to_pydatetime().astimezone(), ratio) for date, ratio in splits.to_dict().items()]
     return splits_list
+
+def get_country(ticker: str) -> Country | None:
+    ticker = __get_ticker_info(ticker)
+
+    try:
+        if ticker.info.get('country'):
+            return Country(ticker.info['country'])
+
+        if ticker.info.get('region'):
+            return Country(ticker.info['region'])
+    except AttributeError:
+        pass
+
+    return None
+
+def get_sector_industry(ticker: str) -> tuple[Sector, str | None]:
+    ticker = __get_ticker_info(ticker)
+
+    try:
+        sector = Sector(ticker.info.get('sector')) if ticker.info.get('sector') else Sector.UNKNOWN
+        industry = ticker.info.get('industry')
+        return sector, industry
+    except AttributeError:
+        return Sector.UNKNOWN, None
