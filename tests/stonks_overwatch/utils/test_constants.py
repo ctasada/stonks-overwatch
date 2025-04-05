cd45d6cf7ccfb911c9f4825e470c952ac94de24a
Carlos Tasada <ctasada@gmail.com>
2025-04-05 14:48:41 +0200

    Diversification: Added sector icons
    Also updated dependencies and added more tests
    
    Note: Django update is limited to 5.1.8. Upgrading to 5.2 seems to break 'django-node-assets'

---
+++ tests/stonks_overwatch/utils/test_constants.py
@@ -0,0 +1,23 @@
+from stonks_overwatch.utils.constants import ProductType, Sector
+
+import pytest
+
+def test_product_type_from_str():
+    for t in ProductType:
+        assert ProductType.from_str(t.value) == t
+
+    assert ProductType.from_str("XXX") == ProductType.UNKNOWN
+
+def test_sector_from_str():
+    for t in Sector:
+        if t == Sector.UNKNOWN:
+            continue
+        assert Sector.from_str(t.value) == t
+        assert t.to_logo() is not None
+
+    assert Sector.from_str(None) == Sector.UNKNOWN
+    assert Sector.UNKNOWN.to_logo() is not None
+
+def test_sector_invalid():
+    with pytest.raises(ValueError):
+        Sector.from_str("INVALID_TYPE")
