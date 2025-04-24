d0d1bad96b1b60914d78d45f2f9643a0f337d335
Carlos Tasada <ctasada@gmail.com>
2025-04-24 08:31:45 +0200

    Internal fixes
    The way 'static' files were managed was not aligned with the best practices

--- src/settings.py
+++ src/settings.py
@@ -146,13 +146,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "stonks_overwatch", STATIC_URL)
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "stonks_overwatch", "static")]
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "stonks_overwatch", "static")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "stonks_overwatch", "staticfiles")]
 
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
